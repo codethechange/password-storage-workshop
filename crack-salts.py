be73eb9695b4f36c45c067c30725acd56fda54de
Drew Gregory <djgregny@gmail.com>
2019-04-05 10:05:48 -0700

    fill in the blanks

--- crack-salts.py
+++ crack-salts.py
@@ -12,15 +12,6 @@ with open('salted-passwords.json') as f:
 cracked_users = []
 from hashlib import sha256
 for i in range(len(d['users'])):
-    salt, hash_val = tuple(d['passwords'][i].split('$'))
-    if i % 10 == 0:
-        print(i)
-    # Generate salted hash for each word
-    for word in words:
-        m = sha256()
-        m.update(str(salt + word).encode('utf-8'))
-        if m.hexdigest() == d['passwords'][i]: # the hash exists! We have found a collision
-            cracked_users.append((d['users'][i], word))
-            break
+    pass # TODO
 print('Cracked ' + str(len(cracked_users)) + ' passwords!')
 print(cracked_users[-10:])
--- hash-salt-passwords.py
+++ hash-salt-passwords.py
@@ -1,16 +1,9 @@
-import pandas as pd
-from hashlib import sha256
-import hashlib, binascii
 import json
-from secrets import choice
 with open('passwords.json') as f:
     d = json.load(f)
 from string import printable
 chars = [c for c in printable if c.isalnum()]
 for i in range(len(d['users'])):
-    salt = ''.join([choice(chars) for _ in range(8)])
-    val = binascii.hexlify(hashlib.hmac('sha256', d['passwords'][i].encode('ascii'), salt.encode('ascii'), 1000000))
-    d['passwords'][i] = salt + '$' + val.decode('ascii')
-    print(d['passwords'][i])
+    pass # TODO
 with open('salted-passwords.json', 'w') as f:
     json.dump(d,f)
